89358afdd3731627795e91ded00f1c745f9ae177
Javier Arenas <javier.arenas@vipera.com>
2020-05-07 01:32:19 +0200

    First draft Angular test

--- Angular/Angular.ts
+++ Angular/Angular.ts
@@ -1,32 +1,37 @@
 // @ts-ignore
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 // @ts-ignore
-import { FormGroup, FormControl } from '@angular/forms';
+import {FormGroup, FormControl} from '@angular/forms';
 
 @Component({
-  selector: 'app-area',
-  template: '' + 
-    'form goes here...' +
-    '<pre>{{ form.value | json }}</pre>',
-  styles: []
+    selector: 'app-area',
+    template: `
+        <div style="display: flex; flex-direction: column; width: 200px">
+            <input type="text" [(ngModel)]="form.value.firstname">
+            <input type="number" [(ngModel)]="form.value.age">
+            <input type="text" [(ngModel)]="form.value.lastname">
+            <input type="text" [(ngModel)]="form.value.twitter">
+        </div>
+        <pre>{{ form.value | json }}</pre>`,
+    styles: []
 })
 
 export class MainAppComponent implements OnInit {
-  form: FormGroup;
-  person = {
-    firstname: 'Coder',
-    age: 25,
-    lastname: 'Byte',
-    twitter: '@coderbyte'
-  };
-  personProps = [];
+    form: FormGroup;
+    person = {
+        firstname: 'Coder',
+        age: 25,
+        lastname: 'Byte',
+        twitter: '@coderbyte'
+    };
+    personProps = [];
 
-  ngOnInit() {
-    const formDataObj = {};
-    for (const prop of Object.keys(this.person)) {
-      formDataObj[prop] = new FormControl(this.person[prop]);
-      this.personProps.push(prop);
+    ngOnInit() {
+        const formDataObj = {};
+        for (const prop of Object.keys(this.person)) {
+            formDataObj[prop] = new FormControl(this.person[prop]);
+            this.personProps.push(prop);
+        }
+        this.form = new FormGroup(formDataObj);
     }
-    this.form = new FormGroup(formDataObj);
-  }
 }
